573d308922f8d45b690988beefdebd32976a1690
Yura Trambitskiy <tr.yura@yahoo.com>
2017-11-24 09:40:44 +0300

    Tidy up functions and add JSDoc to them

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,20 +1,34 @@
-'use strict';
-
+/**
+ * Prepare the extension code and run
+ */
 function init() {
-  const container = document.querySelector('.chess-board-container');
-  if (container) {
-    const input = document.createElement('input');
-    input.addEventListener('keydown', (e) => {
-      if (e.keyCode === 13) {
-        go(input.value);
-        input.value = '';
-        input.focus();
-      }
-    });
-    container.appendChild(input);
+  const boardContainer = document.querySelector('#main-board');
+  if (boardContainer) {
+    boardContainer.appendChild(createInput());
   }
 }
 
+/**
+ * Create the field to handle the moves input
+ * @return {HTMLInputElement} - input handling the user input
+ */
+function createInput() {
+  const input = document.createElement('input');
+  input.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+      go(input.value);
+      input.value = '';
+      input.focus();
+    }
+  });
+  return input;
+}
+
+/**
+ * Handle user input and act in appropriate way
+ * The function uses active board on the screen if there's any
+ * @param  {String} input - input, in format 'e2e4'
+ */
 function go(input) {
   try {
     makeMove(myEvent.capturingBoard, input.slice(0, 2), input.slice(2, 4));
@@ -23,6 +37,13 @@ function go(input) {
   }
 }
 
+/**
+ * Check move and make it if it's legal
+ * This function relies on chess.com chessboard interface
+ * @param  {ChessBoard} board - chessboard instance
+ * @param  {String} fromField - starting field, e.g. 'e2'
+ * @param  {String} toField   - ending field, e.g. 'e4'
+ */
 function makeMove(board, fromField, toField) {
   if (board.gameRules.isLegalMove(board.gameSetup, fromField, toField)) {
       board.fireEvent('onDropPiece', {
