051bbde16c9c160c8274f39a55f7d8e0c687853f
Yura Trambitskiy <tr.yura@yahoo.com>
2017-11-25 18:26:48 +0300

    Add build script

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,62 +1,7 @@
-/**
- * Prepare the extension code and run
- */
-function init() {
-  const selector = '.main-board .board, #chessboard';
-  const boardElement = document.querySelector(selector);
-  if (boardElement) {
-    boardElement.appendChild(createInput());
-  }
-}
-
-/**
- * Create the field to handle the moves input
- * @return {HTMLInputElement} - input handling the user input
- */
-function createInput() {
-  const input = document.createElement('input');
-  input.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
-      go(input.value);
-      input.value = '';
-      input.focus();
-    }
-  });
-  return input;
-}
-
-/**
- * Handle user input and act in appropriate way
- * The function uses active board on the screen if there's any
- * @param  {String} input - input, in format 'e2e4'
- */
-function go(input) {
-  const board = window.myEvent.capturingBoard;
-  if (board) {
-    try {
-      makeMove(board, input.slice(0, 2), input.slice(2, 4));
-    } catch (e) {
-      alert('Move is illegal');
-    }
-  }
-}
-
-/**
- * Check move and make it if it's legal
- * This function relies on chess.com chessboard interface
- * @param  {ChessBoard} board - chessboard instance
- * @param  {String} fromField - starting field, e.g. 'e2'
- * @param  {String} toField   - ending field, e.g. 'e4'
- */
-function makeMove(board, fromField, toField) {
-  if (board.gameRules.isLegalMove(board.gameSetup, fromField, toField)) {
-      board.fireEvent('onDropPiece', {
-          fromAreaId: fromField,
-          targetAreaId: toField,
-      });
-  } else {
-    throw new Error('Move is illegal');
-  }
-}
-
-window.addEventListener('load', init);
+const script = document.createElement('script');
+// %s is replaced during building
+// eslint-disable-next-line quotes
+script.innerHTML = (`%%s%%`);
+window.addEventListener('load', () => {
+  document.body.appendChild(script);
+});
