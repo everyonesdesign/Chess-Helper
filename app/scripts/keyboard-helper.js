1a3e8606168da4e17bcc9b89720e237e2e0d7e16
Yura Trambitskiy <tr.yura@yahoo.com>
2017-12-02 19:36:03 +0300

    Implement move backward/forward functionality with arrows

--- app/scripts/keyboard-helper.js
+++ app/scripts/keyboard-helper.js
@@ -1,3 +1,11 @@
+const KEY_CODES = {
+  enter: 13,
+  leftArrow: 37,
+  topArrow: 38,
+  rightArrow: 39,
+  bottomArrow: 40,
+};
+
 /**
  * Prepare the extension code and run
  */
@@ -11,23 +19,7 @@ function init() {
     input.setAttribute('id', 'ccHelper-input');
     input.className = 'ccHelper-input';
     input.setAttribute('placeholder', 'Enter move here...');
-    input.addEventListener('keydown', (e) => {
-      if (e.keyCode === 13) {
-        go(input.value);
-
-        const board = getBoard();
-        board && board.clearMarkedArrows();
-
-        sendDataToAnalytics({
-          category: 'enter',
-          action: 'press',
-          label: input.value,
-        });
-
-        input.value = '';
-        input.focus();
-      }
-    });
+    input.addEventListener('keydown', handleKeyDown);
     input.addEventListener('input', () => {
       const board = getBoard();
       const move = parseMoveText(input.value);
@@ -44,6 +36,52 @@ function init() {
     messages.className = 'ccHelper-messages';
     document.body.appendChild(messages);
   }
+}
+
+/**
+ * Handle keyDown event on the input
+ * Responsible for submitting move, backward/forward moves, etc.
+ * @param  {Event} e
+ */
+function handleKeyDown(e) {
+  const input = e.target;
+
+  if (e.keyCode === KEY_CODES.enter) {
+    go(input.value);
+
+    const board = getBoard();
+    board && board.clearMarkedArrows();
+
+    sendDataToAnalytics({
+      category: 'enter',
+      action: 'press',
+      label: input.value,
+    });
+
+    input.value = '';
+    input.focus();
+  } else if (holdingCtrlOrCmd(e)) {
+    if (e.keyCode === KEY_CODES.leftArrow) {
+      const selector = '.move-list-buttons .icon-chevron-left, .control-group .icon-chevron-left';
+      document.querySelector(selector).parentNode.click();
+    } else if (e.keyCode === KEY_CODES.rightArrow) {
+      const selector = '.move-list-buttons .icon-chevron-right, .control-group .icon-chevron-right';
+      document.querySelector(selector).parentNode.click();
+    }
+  }
+}
+
+/**
+ * Is user holding Ctrl (on PC) or Cmd (on Mac)
+ * @param {Event} e
+ * @return {Boolean}
+ */
+function holdingCtrlOrCmd(e) {
+  if (navigator.platform === 'MacIntel') {
+    return e.metaKey;
+  }
+
+  return e.ctrlKey;
 }
 
 /**
