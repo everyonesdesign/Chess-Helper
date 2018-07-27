2575ff037957a3509444068fb17deb36513e2360
Yura Trambitskiy <tr.yura@yahoo.com>
2018-07-27 20:01:00 +0200

    Get rid of make.js
    Use browserify/watchify CLI instead

--- make.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const browserify = require('browserify');
-
-// Create a write stream for the pipe to output to
-const bundleFs = fs.createWriteStream('app/build.js');
-
-const b = browserify({standalone: 'nodeModules'});
-b.add('app/src/index.js');
-b.bundle().pipe(bundleFs);
