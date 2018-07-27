d49213efb5b9d6f29ea6c6a33594b051edf4015a
Yura Trambitskiy <tr.yura@yahoo.com>
2018-07-27 11:02:15 +0200

    Implement browserify support

--- make.js
+++ make.js
@@ -1,29 +1,39 @@
 const fs = require('fs');
+const browserify = require('browserify');
 
-fs.readFile('app/src/index.js', 'utf8', (error, source) => {
-  if (error) {
-    return console.log(error);
-  }
+// Create a write stream for the pipe to output to
+const bundleFs = fs.createWriteStream('app/build.js');
 
-  // escape backticks, so we can use them in the source
-  // supports up to 2 layers of backticks
-  // i.e. alert(`Hello, \` world!`);
-  // this is a real magic 😈
-  const escaped = source.replace(/(\\?)`/g, '\\$1$1`');
+const b = browserify({standalone: 'nodeModules'});
+b.add('app/src/index.js');
+b.bundle().pipe(bundleFs);
 
-  const content = `
-    const script = document.createElement('script');
-    script.innerHTML = (\`${escaped}\`);
-    window.addEventListener('load', () => {
-      document.body.appendChild(script);
-    });
-  `;
-
-  fs.writeFile('app/build.js', content, (err) => {
-    if (err) {
-      return console.log(err);
+bundleFs.on('finish', function() {
+  fs.readFile('app/build.js', 'utf8', (error, source) => {
+    if (error) {
+      return console.log(error);
     }
 
-    console.log('Success!');
+    // escape backticks, so we can use them in the source
+    // supports up to 2 layers of backticks
+    // i.e. alert(`Hello, \` world!`);
+    // this is a real magic 😈
+    const escaped = source.replace(/(\\?)`/g, '\\$1$1`');
+
+    const content = `
+      const script = document.createElement('script');
+      script.innerHTML = (\`${escaped}\`);
+      window.addEventListener('load', () => {
+        document.body.appendChild(script);
+      });
+    `;
+
+    fs.writeFile('app/build.js', content, (err) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log('Success!');
+    });
   });
 });
