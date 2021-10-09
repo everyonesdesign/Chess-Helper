110fc766f5176507525ebf39fd762d32999bb997
Yura Trambitckii <tr.yura@yahoo.com>
2021-10-09 20:17:52 +0200

    Add e2e tests to lower algebraic notation

--- cypress/integration/games.spec.js
+++ cypress/integration/games.spec.js
@@ -19,144 +19,24 @@ context('Chess games', () => {
     cy.fixture('positions').as('positions')
   });
 
-  it('Tal-Spassky', function() {
-    const moves = [
-      'c4',
-      'Nf6',
-      'Nc3',
-      'e6',
-      'd4',
-      'c5',
-      'd5',
-      'exd5',
-      'cxd5',
-      'g6',
-      'Nf3',
-      'Bg7',
-      'Bf4',
-      'd6',
-      'h3',
-      'O-O',
-      'e3',
-      'Ne8',
-      'Be2',
-      'Nd7',
-      'O-O',
-      'Ne5',
-      'Bxe5',
-      'dxe5',
-      'Nd2',
-      'f5',
-      'Qb3',
-      'Nd6',
-      'Nc4',
-      'e4',
-      'Nb5',
-      'Nxb5',
-      'Qxb5',
-      'b6',
-      'd6',
-      'Bd7',
-      'Qb3',
-      'b5',
-      'Nb6+',
-      'c4',
-      'Bxc4+',
-      'bxc4',
-      'Qxc4+',
-      'Rf7',
-      'Nxa8',
-      'Qxa8',
-      'Qb3',
-      'Be5',
-      'Rac1',
-      'Kg7',
-      'Rfd1',
-      'a5',
-      'Rc7',
-      'Qe8',
-      'Qd5',
-      'a4',
-      'b4',
-      'axb3',
-      'axb3',
-      'Bf6',
-      'Rb7',
-      'Qe5',
-      'Qc4',
-      'f4',
-      'exf4',
-      'Qxf4',
-      'g3',
-      'Qf3',
-      'Qd5',
-      'Bc3',
-      'Rf1',
-      'Kh6',
-      'Qd1',
-      'Qf6',
-      'Qe2',
-      'Bd4',
-      'Rb4',
-      'e3',
-      'Qd3',
-      'Qxf2+',
-      'Rxf2',
-      'exf2+',
-      'Kh2',
-      'f1=N+',
-      'Qxf1',
-      'Rxf1',
-      'Rxd4',
-      'Rf2+',
-      'Kg1',
-      'Rf3',
-    ];
-
+  xit('Tal-Spassky - algebraic', function() {
+    const position = this.positions['tal-spassky'];
     cy
-      .playGame(moves)
-      .fenEquals(this.positions['tal-spassky'].positions.end)
+      .playGame(position.movesAlgebraic)
+      .fenEquals(position.fen.end)
   });
 
-  it('Opera night', function() {
-    const moves = [
-      'e4',
-      'e5',
-      'Nf3',
-      'd6',
-      'd4',
-      'Bg4',
-      'dxe5',
-      'Bxf3',
-      'Qxf3',
-      'dxe5',
-      'Bc4',
-      'Nf6',
-      'Qb3',
-      'Qe7',
-      'Nc3',
-      'c6',
-      'Bg5',
-      'b5',
-      'Nxb5',
-      'cxb5',
-      'Bxb5+',
-      'Nbd7',
-      'O-O-O',
-      'Rd8',
-      'Rxd7',
-      'Rxd7',
-      'Rd1',
-      'Qe6',
-      'Bxd7+',
-      'Nxd7',
-      'Qb8+',
-      'Nxb8',
-      'Rd8#',
-    ];
-
+  it('Opera night - algebraic', function() {
+    const position = this.positions['opera-night'];
     cy
-      .playGame(moves)
-      .fenEquals(this.positions['opera-night'].positions.end)
+      .playGame(position.movesAlgebraic)
+      .fenEquals(position.fen.end)
+  });
+
+  it('Opera night - algebraic lower', function() {
+    const position = this.positions['opera-night'];
+    cy
+      .playGame(position.movesAlgebraic.map(i => i.toLowerCase()))
+      .fenEquals(position.fen.end)
   });
 });
